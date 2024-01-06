bcce5d1c98be91d0ebdd809f66e79c3a187211ca
JimPap7 <papjim151@gmail.com>
2024-01-05 19:43:42 -0500

    exec

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,25 +6,98 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let userswithsamename = users.filter((user)=>{
+        return user.username === username
+      });
+      if(userswithsamename.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validusers.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let newReview = req.query.body;
+    let usern = session.user;
+    let found = false;
+    if(newReview.length == 0) {
+        return res.status(408).json({message: "Please enter a valid login"});
+    }
+    for (const [key, value] of Object.entries(books)) {
+        if(value.ISBN === isbn) {
+            found = true;
+            books.key.review[usern] = newReview;
+        }
+      }
+      if (found) {
+        return res.status(200).json({message: "review submitted succesfully"});
+        } else {
+          return res.status(408).json({message: "book with ISBN: " + isbn + "not found"});
+        }
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let isbn = req.params.isbn;
+    let newReview = req.query.body;
+    let usern = session.user;
+    let found = false;
+    if(newReview.length == 0) {
+        return res.status(408).json({message: "Please enter a valid login"});
+    }
+    for (const [key, value] of Object.entries(books)) {
+        if(value.ISBN === isbn) {
+            if(value.reviews.haskey(usern)) {
+              found = true;
+              delete books.key.review[usern];
+            } else {
+            return res.status(408).json({message: "Please first write a review for this book"});
+            
+            }
+        }
+      }
+
+  if (found) {
+  return res.status(200).json({message: "review deleted succesfully"});
+  } else {
+    return res.status(408).json({message: "book with ISBN: " + isbn + "not found"});
+  }
+});
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "ISBN":"123456", "reviews": {"text": "decent read"} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales","ISBN":"1234567", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy","ISBN":"12345678", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh","ISBN":"12345674", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job","ISBN":"1234562", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights","ISBN":"12345677", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "ISBN":"1234568","reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice","ISBN":"12345699", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot","ISBN":"1234588", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy","ISBN":"123456709", "reviews": {} }
 }
 
 module.exports=books;
